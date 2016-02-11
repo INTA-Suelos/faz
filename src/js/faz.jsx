d678367453097338d805a4d0647cfe9488df033b
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2016-02-11 01:31:38 -0300

    IndexRoute para / (Inicio o Home)

--- src/js/faz.jsx
+++ src/js/faz.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, hashHistory } from 'react-router'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 
 import App from './components/app'
+import Home from './components/home'
 import Perfiles from './components/perfiles'
 
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path='/' component={App}>
+      <IndexRoute component={Home} />
       <Route path='/perfiles' component={Perfiles}/>
     </Route>
   </Router>,
