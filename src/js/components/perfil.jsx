1ca9b3605ede8c77712b5a2ef87b97b2a676e57f
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2016-02-11 21:13:22 -0300

    Buscar un perfil en json desde la API, implementación normal

--- src/js/components/perfil.jsx
+++ src/js/components/perfil.jsx
@@ -1,7 +1,37 @@
 import React from 'react'
 
 export default class Perfil extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { perfil: { numero: `perfil id ${props.params.id}` } }
+  }
+
+  componentDidMount() {
+    // Hardcodear por ahora dev
+    fetch(`http://localhost:3000/perfiles/${this.props.params.id}`, {
+      mode: 'cors',
+      headers: {
+       'Accept': 'application/json'
+      }
+    }).then(
+      (res) => {
+        // .json() devuelve otro Promise, que encadenamos
+        res.json().then((json) => {
+          this.setState({ perfil: json.perfil })
+        })
+      },
+      () => {
+        this.setState({ perfil: { numero: 'fallo' } })
+      }
+    )
+  }
+
   render() {
-    return <div>Un perfil con id { this.props.params.id }</div>
+    return (
+      <div>
+        <p>Un perfil con id { this.props.params.id }</p>
+        <p>Número: { this.state.perfil.numero }</p>
+      </div>
+    )
   }
 }
