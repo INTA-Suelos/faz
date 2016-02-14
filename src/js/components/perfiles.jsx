75f263bafa0caefa489e7c61d163ea48c9534fb0
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2016-02-13 22:43:47 -0300

    Linkear absolutamente

--- src/js/components/perfiles.jsx
+++ src/js/components/perfiles.jsx
@@ -8,7 +8,7 @@ export default class Perfiles extends React.Component {
         <h2>Lista de perfiles</h2>
 
         <ul>
-          <li><Link to="perfiles/1">perfil 1</Link></li>
+          <li><Link to="/perfiles/1">perfil 1</Link></li>
         </ul>
       </div>
     )
