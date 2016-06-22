4b78ccee2f47b2c7b9ddf0b07778c3f889046915
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2016-06-22 00:24:49 -0300

    Usar functional stateless components cuando sea posible

--- src/js/Components/CuentaNav.jsx
+++ src/js/Components/CuentaNav.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router'
 
-export default class CuentaNav extends React.Component {
-  render() {
-    var registrationLink, loginLink
+const CuentaNav = ({ logueado }) => {
+  var registrationLink, loginLink
 
-    if (this.props.logueado) {
-      registrationLink = <Link to="/usuarios/editar">Preferencias</Link>
-      loginLink = <Link to="/usuarios/salir">Salir</Link>
-    } else {
-      registrationLink = <Link to="/usuarios/registrarse">Registrarse</Link>
-      loginLink = <Link to="/usuarios/entrar">Entrar</Link>
-    }
-
-    return(
-      <nav id="account">
-        <ul role="nav">
-          <li>{registrationLink}</li>
-          <li>{loginLink}</li>
-        </ul>
-      </nav>
-    )
+  if (logueado) {
+    registrationLink = <Link to="/usuarios/editar">Preferencias</Link>
+    loginLink = <Link to="/usuarios/salir">Salir</Link>
+  } else {
+    registrationLink = <Link to="/usuarios/registrarse">Registrarse</Link>
+    loginLink = <Link to="/usuarios/entrar">Entrar</Link>
   }
+
+  return (
+    <nav id="account">
+      <ul role="nav">
+        <li>{registrationLink}</li>
+        <li>{loginLink}</li>
+      </ul>
+    </nav>
+  )
 }
+
+export default CuentaNav
