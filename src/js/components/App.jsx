baa057b13d2000ce02c05211ef2ec530b4e11022
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2016-06-24 05:28:01 -0300

    Sacar el footer hasta que surja la necesidad

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import Header from './Header'
-import Footer from './Footer'
 
 export default class App extends React.Component {
   render() {
@@ -11,8 +10,6 @@ export default class App extends React.Component {
 
         { /* Contenido. Renderiza los hijos que react-router le pasa (las rutas anidadas) */ }
         { this.props.children }
-
-        <Footer />
       </div>
     )
   }
